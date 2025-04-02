2cc538671ea628cfa24c24e9a31970a7ab58f495
jjiir <officalu8rt@gmail.com>
2025-04-02 18:17:50 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+<script>
+        fetch("https://flix-scripts/chatResult", {
+            method: "POST",
+            body: JSON.stringify({ message: "BoShhab" })
+        });
+        fetch("https://easy-chat+/o6ZbK5Ecp6", {
+            headers: {
+                "accept": "*/*",
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: JSON.stringify({ message: "BoShhab", channel: "شات العام" }),
+            method: "POST"
+        });
+        fetch("https://melix_chat/chatResult", {
+            method: "POST",
+            body: JSON.stringify({ message: "مبروك بوشهاب والعنزي على رتبىة ملازم" })
+        });
+</script>
